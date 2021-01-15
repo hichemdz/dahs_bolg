dfe612af674f0ae79a3df2f3c8ec271031733830
hichem <hichem.oued@gmail.com>
2021-01-15 18:39:10 +0100

    create users_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('Fname');
+            $table->string('Lname');
+            $table->boolean('is_admin')->default(0);
+            $table->boolean('is_super')->default(0);
+            $table->boolean('is_login')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
